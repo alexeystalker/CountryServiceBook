8eb5019f10069b85a18014b3403ef57f58c4db0a
Alexey Mukhachev <alex.mukhachev@skbkontur.ru>
2023-04-01 12:52:42 +0500

    Add gRPC reflection

--- CountryService/CountryService.Web/Program.cs
+++ CountryService/CountryService.Web/Program.cs
@@ -14,11 +14,13 @@ builder.Services.AddGrpc(options =>
     options.ResponseCompressionAlgorithm = "br"; //задаём grpc-accept-encoding, соответствует указанному в провайдере
     options.ResponseCompressionLevel = CompressionLevel.Optimal;
 });
+builder.Services.AddGrpcReflection();
 
 builder.Services.AddSingleton<CountryManagementService>();
 
 var app = builder.Build();
 
+app.MapGrpcReflectionService();
 app.MapGrpcService<CountryGrpcService>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
