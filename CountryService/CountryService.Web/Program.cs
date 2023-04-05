7e6643ebfab1004d6bcfd195c50ece967ece759a
Alexey Mukhachev <alex.mukhachev@skbkontur.ru>
2023-04-05 20:43:36 +0500

    Perform message validation

--- CountryService/CountryService.Web/Program.cs
+++ CountryService/CountryService.Web/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System.IO.Compression;
-using Azure.Core;
+using Calzolari.Grpc.AspNetCore.Validation;
 using CountryService.gRPC.Compression;
 using CountryService.Web;
 using CountryService.Web.Interceptors;
 using CountryService.Web.Services;
-using Grpc.Core;
+using CountryService.Web.Validator;
 using Grpc.Net.Compression;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,14 +12,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc(options =>
 {
     options.EnableDetailedErrors = true;
-    options.IgnoreUnknownServices = true;
     options.MaxReceiveMessageSize = 6291456; // 6 Mb
     options.MaxSendMessageSize = 6291456; // 6 Mb
     options.CompressionProviders = new ICompressionProvider[] { new BrotliCompressionProvider(CompressionLevel.Optimal) };
     options.ResponseCompressionAlgorithm = "br"; //задаём grpc-accept-encoding, соответствует указанному в провайдере
     options.ResponseCompressionLevel = CompressionLevel.Optimal;
     options.Interceptors.Add<ExceptionInterceptor>(); //регистрируем наш перехватчик
+    options.EnableMessageValidation(); //Метод расширения из пакета!
 });
+builder.Services.AddGrpcValidation(); //Добавляем сервисы валидации
+builder.Services.AddValidator<CountryCreateRequestValidator>(); //Добавляем сам валидатор
+
 builder.Services.AddGrpcReflection();
 
 builder.Services.AddSingleton<CountryManagementService>();
@@ -30,23 +33,5 @@ app.MapGrpcReflectionService();
 app.MapGrpcService<CountryGrpcService>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
-app.Use(async(context, next) =>
-{
-    if (context.Request.ContentType == "application/grpc")
-    {
-        context.Response.OnStarting(() =>
-        {
-            if (context.Response.StatusCode == 404)
-            {
-                context.Response.ContentType = "application/grpc";
-                context.Response.Headers.Add("grpc-status", ((int)StatusCode.NotFound).ToString());
-                context.Response.StatusCode = 200; //Помним о том, что HTTP-код должен быть 200 OK
-            }
-            return Task.CompletedTask;
-        });
-    }
-    await next(context);
-});
 
 app.Run();
---
+++ CountryService/CountryService.Web/Validator/CountryCreateRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using CountryService.Web.gRPC;
+using FluentValidation;
+
+namespace CountryService.Web.Validator;
+
+public class CountryCreateRequestValidator: AbstractValidator<CountryCreationRequest>
+{
+    public CountryCreateRequestValidator()
+    {
+        RuleFor(request => request.Name).NotEmpty().WithMessage("Name is mandatory.");
+        RuleFor(request => request.Description).MinimumLength(5)
+            .WithMessage("Description is mandatory and should be longer than 4 characters");
+    }
+}
