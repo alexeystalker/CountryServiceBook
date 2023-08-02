72feb5b832a4ccbc884aef0c85828df99d733433
Alexey Mukhachev <alex.mukhachev@skbkontur.ru>
2023-08-02 19:23:34 +0500

    Добавили компонент Edit

--- CountryService/CountryWiki.Angular/src/app/app.module.ts
+++ CountryService/CountryWiki.Angular/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
